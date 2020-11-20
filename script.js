cbd50078d1fada2cc5fb9fa7a74438e4cec9b707
mejer404 <61515267+mejer404@users.noreply.github.com>
2020-11-20 19:14:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,2 @@
 let page = document.qurySelector('.page');
 page.classList.add('dark-theme');
-let themeButton = document.querSelector('.theme-button');
-themeButton.onclock= function() {
-    page.classList.toggle('dark-theme');
-};
